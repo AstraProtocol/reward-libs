a9bd879e648635ccd0324e20ddf2b972eeb809bf
hoanguyenkh <hoank@astranaut.io>
2023-05-06 10:24:22 +0700

    fix: add crypto aes

---
+++ crypto/aes_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const KEY = "214111111113ptLJq3JrZMCMMNNdL93e"
+
+func TestEncryptDecrypt(t *testing.T) {
+	encryptTxt, err := Encrypt(KEY, "hoank")
+	assert.Nil(t, err)
+	// Cipher key must be 32 chars long because block size is 16 bytes
+	val, err := Decrypt(KEY, encryptTxt)
+	assert.Nil(t, err)
+	assert.Equal(t, "hoank", val)
+}
